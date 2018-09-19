7e7a0a51d30594ad35d44c10b5ed8fa271a893fb
vagrant <vagrant@homestead>
2018-09-19 06:44:17 +0000

    database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(TopicsTableSeeder::class);
+        $this->call(RepliesTableSeeder::class);
+        $this->call(FriendLinkSeeder::class);
     }
 }
