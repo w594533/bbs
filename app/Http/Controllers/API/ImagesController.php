34e69635adf34165ce49e5f3405149236c7f0253
vagrant <vagrant@homestead>
2018-11-16 09:24:39 +0000

    增加上传文件测试&跨域解决

--- app/Http/Controllers/API/ImagesController.php
+++ app/Http/Controllers/API/ImagesController.php
@@ -13,21 +13,37 @@ class ImagesController extends Controller
 {
     public function store(ImageRequest $request, ImageUploadHandler $uploader, Image $image)
     {
-      if ($request->image) {
-        $type = $request->type;
-        $max_width = '';
-        if ($type == 'avatar') {
-          $max_width = 300;
+        if ($request->image) {
+            $type = $request->type;
+            $max_width = '';
+            if ($type == 'avatar') {
+                $max_width = 300;
+            }
+            $path = $uploader->save($request->image, $type, $this->user()->id, $max_width);
+            if ($path) {
+                $image->user_id = $this->user()->id;
+                $image->path = $path;
+                $image->type = $type;
+                $image->save();
+                return $this->response->item($image, new ImageTransFormer)->setStatusCode(201);
+            }
         }
-        $path = $uploader->save($request->image, $type, $this->user()->id, $max_width);
-        if ($path) {
-          $image->user_id = $this->user()->id;
-          $image->path = $path;
-          $image->type = $type;
-          $image->save();
-          return $this->response->item($image, new ImageTransFormer)->setStatusCode(201);
-        }
-      }
+    }
 
+    public function upload(Request $request, ImageUploadHandler $uploader, Image $image)
+    {
+        if ($request->hasFile('file')) {
+            \Log::info($request->file);
+            $path = $uploader->save($request->file, 'test', 'deploy_test');
+            if ($path) {
+                // $image->user_id = $this->user()->id;
+                $image->path = $path;
+                $image->type = 'topic';
+                $image->save();
+                return $this->response->item($image, new ImageTransFormer)->setStatusCode(201);
+            }
+            return $this->response->noContent();
+        }
+        $this->response->error('请上传文件', 422);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,56 +22,59 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
   'namespace' => 'App\Http\Controllers\API',
   'middleware' => ['serializer:array', 'bindings', 'throttle:60,1']
-], function($api) {
-  //获取验证码
-  $api->post('verify_code', 'VerificationCodesController@store')->name('api.verify_code.store');
+], function ($api) {
+    //获取验证码
+    $api->post('verify_code', 'VerificationCodesController@store')->name('api.verify_code.store');
 
-  //用户注册，通过手机号，验证码
-  $api->post('users', 'UsersController@store')->name('api.users.store');
+    //用户注册，通过手机号，验证码
+    $api->post('users', 'UsersController@store')->name('api.users.store');
 
-  //第三方登录
-  $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialsStore')->name('api.authorizes.socialsStore');
+    //第三方登录
+    $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialsStore')->name('api.authorizes.socialsStore');
 
-  //话题列表
-  $api->get('topics', 'TopicsController@index')->name('api.topics.index');
-  //某个用户话题列表
-  $api->get('user/{user}/topics', 'TopicsController@userIndex')->name('api.user.topics.index');
-  //详情
-  $api->get('topics/{topic}', 'TopicsController@show')->name('api.topics.show');
+    //话题列表
+    $api->get('topics', 'TopicsController@index')->name('api.topics.index');
+    //某个用户话题列表
+    $api->get('user/{user}/topics', 'TopicsController@userIndex')->name('api.user.topics.index');
+    //详情
+    $api->get('topics/{topic}', 'TopicsController@show')->name('api.topics.show');
 
-  $api->get('categories', 'CategoriesController@index')->name('api.categories.index');
+    $api->get('categories', 'CategoriesController@index')->name('api.categories.index');
 
-  //回复列表
-  $api->get('replies', 'RepliesController@index')->name('api.replies.index');
-  //某个有用户的回复列表
-  $api->get('user/{user}/replies', 'RepliesController@userIndex')->name('api.user.replies.index');
+    //回复列表
+    $api->get('replies', 'RepliesController@index')->name('api.replies.index');
+    //某个有用户的回复列表
+    $api->get('user/{user}/replies', 'RepliesController@userIndex')->name('api.user.replies.index');
 
-  //边栏活跃用户
-  $api->get('actived_users', 'UsersController@activedUsers')->name('api.user.actived_users');
+    //边栏活跃用户
+    $api->get('actived_users', 'UsersController@activedUsers')->name('api.user.actived_users');
 
-  //友情链接
-  $api->get('friend_links', 'FriendLinksController@index')->name('api.friend_links.index');
+    //友情链接
+    $api->get('friend_links', 'FriendLinksController@index')->name('api.friend_links.index');
 
-  $api->group(['middleware' => 'auth:api'], function($api) {
-      // 当前登录用户信息
-      $api->get('/users', 'UsersController@me')->name('api.users.show');
+    //api上传文件测试
+    $api->post('/upload', 'ImagesController@upload')->name('api.images.upload');
 
-      //图片资源
-      $api->post('/images', 'ImagesController@store')->name('api.images.store');
+    $api->group(['middleware' => 'auth:api'], function ($api) {
+        // 当前登录用户信息
+        $api->get('/users', 'UsersController@me')->name('api.users.show');
 
-      //修改用户信息
-      $api->patch('/users', 'UsersController@update')->name('api.users.update');
+        //图片资源
+        $api->post('/images', 'ImagesController@store')->name('api.images.store');
 
-      //话题相关
-      $api->resource('topics', 'TopicsController', ['only' => ['store', 'update', 'destroy']]);
+        //修改用户信息
+        $api->patch('/users', 'UsersController@update')->name('api.users.update');
 
-      //回复
-      $api->post('topics/{topic}/replies', 'RepliesController@store')->name('api.replies.store');
-      $api->delete('replies/{reply}', 'RepliesController@destroy')->name('api.replies.destroy');
+        //话题相关
+        $api->resource('topics', 'TopicsController', ['only' => ['store', 'update', 'destroy']]);
 
-      //消息通知列表
-      $api->get('user/notifications', 'NotificationsController@userIndex')->name('api.user.notifications.index');
-      //标记消息已读
-      $api->patch('user/read/notifications', 'NotificationsController@read')->name('api.user.notifications.read');
-  });
+        //回复
+        $api->post('topics/{topic}/replies', 'RepliesController@store')->name('api.replies.store');
+        $api->delete('replies/{reply}', 'RepliesController@destroy')->name('api.replies.destroy');
+
+        //消息通知列表
+        $api->get('user/notifications', 'NotificationsController@userIndex')->name('api.user.notifications.index');
+        //标记消息已读
+        $api->patch('user/read/notifications', 'NotificationsController@read')->name('api.user.notifications.read');
+    });
 });
